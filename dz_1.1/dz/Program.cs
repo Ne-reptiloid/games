c6a64998b7a55d43fc9087c0a5b87a34b908cc72
Ne-reptiloid <nikshidalla@mail.ru>
2024-11-29 11:19:58 +0300

    добавил дз1.1

---
+++ dz_1.1/dz/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите число: ");
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 2; i <= n; i++)
+        {
+            if (IsPrime(i))
+                Console.WriteLine(i);
+        }
+
+        Console.WriteLine("\nНажмите любую клавишу для выхода...");
+        Console.ReadKey();
+    }
+
+    static bool IsPrime(int number)
+    {
+        if (number < 2) return false;
+
+        for (int j = 2; j * j <= number; j++)
+        {
+            if (number % j == 0)
+                return false;
+        }
+
+        return true;
+    }
+}
