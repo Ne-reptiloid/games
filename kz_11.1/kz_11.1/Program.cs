89719c698ab3b34bc0fdeccc4e634292b2560723
Ne-reptiloid <nikshidalla@mail.ru>
2024-12-14 00:52:03 +0300

    Добавил КЗ_1.11

---
+++ kz_11.1/kz_11.1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        // Создаем объект StringBuilder для хранения данных
+        var sb = new StringBuilder();
+
+        // Добавляем заголовок
+        sb.AppendLine("x\tsin(x)");
+
+        // Шаг изменения x
+        double step = 0.1;
+
+        for (double x = 0; x <= 1; x += step)
+        {
+            // Вычисление значения функции sin(x)
+            double sinX = Math.Sin(x);
+
+            // Форматируем строку и добавляем её в StringBuilder
+            sb.AppendFormat("{0}\t{1}\n", x.ToString("F2"), sinX.ToString("F4"));
+        }
+
+        // Записываем данные в файл f.txt
+        string filePath = Path.Combine(Directory.GetCurrentDirectory(), "f.txt");
+        File.WriteAllText(filePath, sb.ToString());
+
+        // Открываем созданный файл
+        Process.Start(new ProcessStartInfo(filePath) { UseShellExecute = true });
+
+        Console.WriteLine("Таблица значений записана в файл 'f.txt' и открыт.");
+    }
+}
---
+++ kz_11.1/kz_11.1/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("kz_11.1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("kz_11.1")]
+[assembly: AssemblyCopyright("Copyright © HP 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("9d93aeaa-8324-4d80-b22c-b54a60d5b9f5")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
